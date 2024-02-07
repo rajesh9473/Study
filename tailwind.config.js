278a380d54125072ed2a037046a56baa2fe20c01
rajesh9473 <149662452+rajesh9473@users.noreply.github.com>
2024-02-07 11:15:48 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT", "@material-tailwind/html/utils/withMT");
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
 module.exports = withMT({
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [],
 });
